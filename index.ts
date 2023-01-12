6a6bfcacc1c0468afe08ead83a004bac85919f1e
Chris Gemmell <fablekey@gmail.com>
2023-01-13 08:48:41 +1100

    1 line for both vite and react

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 import { exec } from 'node:child_process';
 import fs from 'node:fs/promises';
-import { kill } from 'node:process';
 import viteScripts from './lib';
 
 async function readPackageJson() {
-  const file = await fs.readFile('./cra/package.json', 'utf-8');
+  const file = await fs.readFile('./package.json', 'utf-8');
   return JSON.parse(file);
 }
 
 async function start() {
   const pkg = await readPackageJson();
   pkg.scripts = viteScripts;
-  exec('npm install --save-dev vite', (err, stdout, stderr) => {
+  exec('npm install --save-dev vite @vitejs/plugin-react', (err, stdout, stderr) => {
   if (err) {
       console.error(err);
       return;
@@ -19,14 +18,14 @@ async function start() {
     console.log(stdout);
     console.log(stderr);
   });
-  exec('npm install --save-dev @vitejs/plugin-react', (err, stdout, stderr) => {
+  exec('npm install --save react@latest react-dom@latest', (err, stdout, stderr) => {
   if (err) {
       console.error(err);
       return;
     }
     console.log(stdout);
     console.log(stderr);
-  })
+  });
   // await fs.writeFile('./cra/package.json', JSON.stringify(pkg, null, 2));
 
 }
